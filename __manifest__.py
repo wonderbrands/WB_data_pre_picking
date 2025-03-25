9b784059363c1fcaebd228b729012105ab5e8448
Niffy-the-conqueror <117080364+Niffy-the-conqueror@users.noreply.github.com>
2025-03-25 19:38:17 +0100

    feat: modified to be installed in v18

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory',
-    'version': '15.0',
+    'version': '18.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
